48107640f22c6c9f33b875ba279c68e0f2e5cce6
Kelvin Lucas <lucaskelvinbsantos@outlook.com>
2022-06-27 23:06:29 -0300

    entendendo melhor interfaces, levado ao conceito de classes anonimas para entender a instanciacao de interfaces

--- src/execut/Util.java
+++ src/execut/Util.java
@@ -3,12 +3,16 @@ package execut;
 import java.util.Date;
 
 import equipaments.Impressora;
+import groupInterface.Copiadora;
 import groupInterface.Fax;
 import groupInterface.Multifuncional;
 import groupInterface.Printer;
 import groupInterface.Scanner;
 
 public class Util {
+	
+	
+	//1
 	private static Impressora impressora;
 	
 	private static Impressora getImpressoraInstance() {
@@ -23,22 +27,25 @@ public class Util {
 		return impressora;
 	}
 	
+	//2
 	public static Scanner getScannerAtual() {
 		return getImpressoraInstance();
 	}
 	
+	//3
 	public static Fax getFaxAtual() {
 		return getImpressoraInstance();
 	}
-	
-	public static Fax getCopiadora() {
+	//4
+	public static Copiadora getCopiadora() {
 		return getImpressoraInstance();
 	}
 	
+	//5
 	public static Printer getPrinter() {
 		return getImpressoraInstance();
 	}
-	
+	//6
 	public static Multifuncional getImpressora() {
 		return getImpressoraInstance();
 	}
--- src/execut/home.java
+++ src/execut/home.java
@@ -2,24 +2,30 @@ package execut;
 
 import java.awt.image.BufferedImage;
 
+import equipaments.Impressora;
 import groupInterface.Copiadora;
 import groupInterface.Fax;
+import groupInterface.Multifuncional;
 import groupInterface.Printer;
 import groupInterface.Scanner;
 
 public class home {
 	public static void main (String[] args) {
-		
+	
+	//1
 	Scanner scanner = Util.getScannerAtual();
 	BufferedImage imagemDigitalizada = scanner.digitalizar();
-	
+
+	//2
 	Fax fax = Util.getFaxAtual();
 	fax.enviarFax("Hell World", "1996");
 	String faxRecebido = fax.receberFax("1996");
 	
+	//3
 	Copiadora cop = Util.getCopiadora();
 	cop.copiarDocumento(10);
 	
+	//4
 	Printer printer = Util.getPrinter();
 	String dados = "Hello World";
 	printer.imprimir(dados.getBytes(), 1);
